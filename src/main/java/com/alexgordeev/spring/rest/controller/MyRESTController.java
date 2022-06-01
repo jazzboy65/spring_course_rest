efa1baea4965dcff2ffda9b92da936829fab4d76
jazzboy65 <alexis.gordeeff@gmail.com>
2022-06-01 17:59:14 +0500

    scr 2-7

---
+++ src/main/java/com/alexgordeev/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,35 @@
+package com.alexgordeev.spring.rest.controller;
+
+import com.alexgordeev.spring.rest.entity.Employee;
+import com.alexgordeev.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+    }
+}
